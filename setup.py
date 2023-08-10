131829aa17e47ed91ae3de05dc36b2cb0afcdd53
José Mª Fernández <josemariafg@gmail.com>
2023-08-10 13:44:07 +0200

    Added shellcheck to the bundle, so the validation is complete

--- setup.py
+++ setup.py
@@ -8,29 +8,45 @@ import tempfile
 import urllib.request
 
 setupDir = os.path.dirname(__file__)
-with open(os.path.join(setupDir, ".version")) as vH:
-    version = vH.read().strip()
+with open(os.path.join(setupDir, ".version.actionlint")) as vH:
+    actionlint_version = vH.read().strip()
+
+with open(os.path.join(setupDir, ".version.shellcheck")) as vH:
+    shellcheck_version = vH.read().strip()
 
 machine = platform.machine()
 if machine == "x86_64":
-    machine = "amd64"
+    go_machine = "amd64"
 elif machine == "aarch64":
-    machine = "arm64"
+    go_machine = "arm64"
+else:
+    go_machine = machine
 system = platform.system().lower()
-download_link = f"https://github.com/rhysd/actionlint/releases/download/v{version}/actionlint_{version}_{system}_{machine}.tar.gz"
-local_filename, headers = urllib.request.urlretrieve(download_link)
-with tarfile.open(local_filename, mode="r:*") as tF:
+actionlint_download_link = f"https://github.com/rhysd/actionlint/releases/download/v{actionlint_version}/actionlint_{actionlint_version}_{system}_{go_machine}.tar.gz"
+shellcheck_download_link = f"https://github.com/koalaman/shellcheck/releases/download/v{shellcheck_version}/shellcheck-v{shellcheck_version}.{system}.{machine}.tar.xz"
+
+local_actionlint_archive, headers = urllib.request.urlretrieve(actionlint_download_link)
+with tarfile.open(local_actionlint_archive, mode="r:*") as tF:
     edir = tempfile.mkdtemp()
     atexit.register(shutil.rmtree, edir)
     tF.extractall(edir)
-    the_path = os.path.join(edir, "actionlint")
+    the_actionlint_path = os.path.join(edir, "actionlint")
     # Assuring the right permissions
-    os.chmod(the_path, 0o555)
+    os.chmod(the_actionlint_path, 0o555)
+
+local_shellcheck_archive, headers = urllib.request.urlretrieve(shellcheck_download_link)
+with tarfile.open(local_shellcheck_archive, mode="r:*") as tF:
+    edir = tempfile.mkdtemp()
+    atexit.register(shutil.rmtree, edir)
+    tF.extractall(edir)
+    the_shellcheck_path = os.path.join(edir, f"shellcheck-v{shellcheck_version}", "shellcheck")
+    # Assuring the right permissions
+    os.chmod(the_shellcheck_path, 0o555)
 
 setup(
     name='pre_commit_placeholder_package',
-    version=version,
+    version=actionlint_version,
     data_files=[
-        ("bin", [the_path])
+        ("bin", [the_actionlint_path, the_shellcheck_path])
     ]
 )
